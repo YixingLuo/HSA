ba50ae8153a7cdfd3679049f0c653dd9d7ef5fbd
Tortoise097 <54835950+Tortoise097@users.noreply.github.com>
2022-05-18 12:57:00 +0800

    update codes

--- Codes/ConfigurationRankingbyHSA.py
+++ Codes/ConfigurationRankingbyHSA.py
@@ -8,90 +8,12 @@ Compare_count = 0
 df_compare_record = pd.DataFrame(columns=['A', 'B', 'Compare_result', 'K', 'Ms', 'j', 'Dis']) 
 rootdir = ''
 
-
-# This function(ComparebyImportanceClass) compare 2 results by their Importance class.
-# Note that the imput parameters(Ipc1, Ipc2) are already Importance class
-# The returned result t is -1, 0 , 1; -1 means Ipc1 is severer than Ipc2,
-# while 1 means that Ipc2 is severer. if t = 0, it means we can not distinguish
-# which one is severer in this level(importance class)
-def ComparebyImportanceClass(Ipc1, Ipc2): # sort a "Importance class" [0:k]
-    result = 0
-    for i in range(len(Ipc1)):
-        if Ipc1[i] > Ipc2[i]:
-            result = -1
-            break
-        elif Ipc1[i] < Ipc2[i]:
-            result = 1
-            break
-    return result
-
-# Severity compare
-
-# This function(s_to_impl) calculate a result's Importance level by a given pattern
-# input parameters: result(Severity) and given pattern(Pattern)
-# output: the result's importance level
-# Note that the given pattern should be a dict, in our case it is {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
-# N_req: the number of requirements in results, in out case it is 10
-def s_to_impl(Severity, Pattern, N_req): # severity to ImpL                                                                                    
-    p = [0 for x in range(0,N_req)]
-    for i in range(N_req):
-        if Severity[i] == 0:
-            p[i] = 0
-        else:
-            p[i] = 1
-    N_imp = len(Pattern)
-    l = [0 for x in range(0,N_imp)]
-    for i in range(N_imp):
-        for j in range(len(Pattern[i])):
-            l[i] += p[Pattern[i][j]]
-    return tuple(l)
-
-# This function(calc_sv) calculate a result's Severity by a given pattern(Pattern)
-# input parameters: result(s) and given pattern(Pattern)
-# output: the result's Severity(sv)
-# Note that the given pattern should be a dict, in our case it is {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
-# N_req: the number of requirements in results, in out case it is 10
-def calc_sv(s,Pattern):
-    N_imp = len(Pattern)
-    sv = [0 for x in range(0,N_imp)]
-    for i in range(N_imp):
-        for j in range(len(Pattern[i])):
-            sv[i] += s[Pattern[i][j]]
-    return sv
-
-# This function(sv_add_s) add a result(s2) to a severity(s1), and return the overall severity
-def sv_add_s(s1,s2, Pattern):
-    N_imp = len(Pattern)
-    for i in range(N_imp):
-        for j in range(len(Pattern[i])):
-            s1[i] += s2[Pattern[i][j]]
-    return s1
-
-# This function(comp_sv) compare 2 severity(sv1 and sv2), the returned result is a list:
-# result[0] is the compare result:-1 means sv1 is severer,1 means the otherwise, 0 means sv1 and sv2 are tied
-# result[1] records in which layer sv1 and sv2 are distinguished
-# result[2] records sv1 and sv2's differneces in the layer in which they are distinguished
-def comp_sv(sv1,sv2):
-    result = [0,len(sv1),0]
-    # print("compare_sv:",sv1,sv2)
-    for i in range(len(sv1)):
-        if sv1[i] > sv2[i]:
-            result[0] = -1
-            result[1] = i
-            result[2] = sv1[i] - sv2[i]
-            break
-        elif sv1[i] < sv2[i]:
-            result[0] = 1
-            result[1] = i
-            result[2] = sv2[i] - sv1[i]
-            break
-    return result
-
+from HierarchicalSafetyAssessment import ComparebyImportanceClass, s_to_impl, calc_sv, sv_add_s, comp_sv
 
 # This function(getDF) read the recorded result and return a dataframe
 def getDF(folder):
     df = pd.DataFrame(columns=['Severity'])
-    file = folder + '/A_resultMod.txt'
+    file = folder + '/Requirements_Violation_Severity.txt'
     result = np.loadtxt(file)
     for i in range(10000): # len(fileList); 1000-5000-10000
         df.loc[i,'Severity'] = result[i]
@@ -115,8 +37,13 @@ def Comp2ADSbySeverity(ADS1, ADS2):
     
     impl_sv1 = {}
     impl_sv2 = {}
-    N_req = 10
-    Pattern = {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
+    
+    f = open(r"E:\UAV\mazda_PP\Hierarchical-Safety-Assessment\Results_RVA\Requirements_Level_Pattern.txt",encoding = "utf-8")
+    f2 = open(r"E:\UAV\mazda_PP\Hierarchical-Safety-Assessment\Results_RVA\N_req.txt",encoding = "utf-8")
+    
+    N_req = eval(f2.read())
+    Pattern = eval(f.read())
+    
     for s in sv1:
         impl = s_to_impl(s,Pattern,N_req)
         if impl_sv1.get(impl) == None:
@@ -131,9 +58,9 @@ def Comp2ADSbySeverity(ADS1, ADS2):
         else:
             impl_sv2[impl] = sv_add_s(impl_sv2[impl], s, Pattern)
             
-    N_class = 4
+    N_class = len(Pattern)
     result = 0
-    LEVEL = 4
+    LEVEL = len(Pattern)
     g1 = list(impl_sv1.keys())
     g2 = list(impl_sv2.keys())
     Ms = []
@@ -146,8 +73,8 @@ def Comp2ADSbySeverity(ADS1, ADS2):
         g = list(set(g))
         g = sorted(g, key=cmp_to_key(ComparebyImportanceClass))
         for t in g:
-            sum_s1 = [0,0,0,0]
-            sum_s2 = [0,0,0,0]
+            sum_s1 = [0 for x in range(0,N_class)]
+            sum_s2 = [0 for x in range(0,N_class)]
             for j in impl_sv1.keys():
                 if j[0:(i+1)] == t:
                     for jj in range(i+1):
@@ -177,39 +104,69 @@ def Comp2ADSbySeverity(ADS1, ADS2):
 # and record the compare result in excels
 def SortADS(fname, compfolder):
     global df_compare_record
+    pd.set_option('display.max_columns',None)
+    pd.set_option('display.max_rows',None)
+    
     ADS_list = os.listdir(compfolder)
 
     ADS_list = sorted(ADS_list, key=cmp_to_key(Comp2ADSbySeverity))
     
     
-    df = pd.DataFrame(columns=['Scenario'])
-    
+    df = pd.DataFrame(columns=['Scenario','rank'])
+    ADS_list.reverse()
     df['Scenario'] = ADS_list
     
+    # print(df)
+    # print(df_compare_record)
+    
+    N_ADS = len(ADS_list)
+    rank_count = 1
+    rank_now = 1
+    # df['rank'][0] = rank_now
+    df.loc[0, 'rank'] = rank_now
+
+    for i in range(1,N_ADS):
+        query_stc = 'A == ' + "\'" + df.loc[i-1,'Scenario'] + "\'" + ' and B == ' + "\'" + df.loc[i,'Scenario']  + "\'"
+        l = df_compare_record.query(query_stc)
+        if l.empty == True:
+            query_stc = 'A == ' + "\'" + df.loc[i,'Scenario'] + "\'" + ' and B == ' + "\'" + df.loc[i-1,'Scenario']  + "\'"
+            l = df_compare_record.query(query_stc)
+        
+        l_dis = l.iloc[0]['Dis']
+        if(l_dis == 0):
+            rank_count += 1
+        else:
+            rank_count += 1
+            rank_now = rank_count
+        df.loc[i, 'rank'] = rank_now
+        
     # * represent an address
-    writer2 = pd.ExcelWriter('*' + fname + '_Severity_Sort_list.xlsx',engine = 'xlsxwriter')
+    result_folder = '*/Hierarchical-Safety-Assessment/test_result/rank_list/'
+    writer2 = pd.ExcelWriter(result_folder + 'Rankindlist_' + fname + '.xlsx',engine = 'xlsxwriter')
     df.to_excel(writer2,sheet_name = 'Sheet1')
     writer2.save()
     
-    writer3 = pd.ExcelWriter('**' + fname + '_Severity_Compare_process_record.xlsx',engine = 'xlsxwriter')
-    df_compare_record.to_excel(writer3,sheet_name = 'Sheet1')
-    writer3.save()
+    # writer3 = pd.ExcelWriter(result_folder + fname + '_Severity_Compare_process_record.xlsx',engine = 'xlsxwriter')
+    # df_compare_record.to_excel(writer3,sheet_name = 'Sheet1')
+    # writer3.save()
 
     global Compare_count
   
     
 
 if __name__ == '__main__':
-    datafolder = '***'
-    # folder_list = ['CarOppositeLane_Datalog_2021_12_26_21_25', 'ParkedCarInFrontCarOppositeLane_Datalog_2021_12_26_21_27', 'RightTurn_Datalog_2021_12_26_21_27']
-    folder_list = ['***']
-    Scenario = ['BlindIntersection','CarOppositeLane', 'CloseToCrash', 'LeftAndRight', 'ParkedCarInFrontCarOppositeLane', 'RightTurn']
+    # datafolder = 'E:/UAV/mazda_PP/Hierarchical-Safety-Assessment/TESTDATA'
+    # folder_list = ['BlindIntersection']
+    datafolder = '*/Hierarchical-Safety-Assessment/Results_RVA'
+    folder_list = os.listdir(datafolder)
+    folder_list.remove('README.md')
+    folder_list.remove('Requirements_Level_Pattern.txt')
+    folder_list.remove('N_req.txt')
+    
     for f in folder_list:
         print(datafolder + '/' + f)
-        rootdir = datafolder + '/' + f + '/New_Revised'
-        SortADS(f, datafolder + '/' + f + '/New_Revised')
+        rootdir = datafolder + '/' + f
+        SortADS(f, datafolder + '/' + f)
         df_compare_record = df_compare_record.drop(index = df_compare_record.index)
         Compare_count = 0
-
-    
 
--- Codes/HierarchicalSafetyAssessment.py
+++ Codes/HierarchicalSafetyAssessment.py
@@ -10,8 +10,86 @@ df_compare_record = pd.DataFrame(columns=['A', 'B', 'Compare_result', 'K', 'Ms',
 # j: distingguished in which bit of the mode
 # dis: |SV1[j] - SV2[j]|
 
-from ConfigurationRankingbyHSA import ComparebyImportanceClass, s_to_impl, calc_sv, sv_add_s, comp_sv
+# This file comapre each two ADS's(configuration) performance under the same scenario 
+# and record the comparison result in excels
 
+# This function(ComparebyImportanceClass) compare 2 results by their Importance class.
+# Note that the imput parameters(Ipc1, Ipc2) are already Importance class
+# The returned result t is -1, 0 , 1; -1 means Ipc1 is severer than Ipc2,
+# while 1 means that Ipc2 is severer. if t = 0, it means we can not distinguish
+# which one is severer in this level(importance class)
+def ComparebyImportanceClass(Ipc1, Ipc2): # sort a "Importance class" [0:k]
+    result = 0
+    for i in range(len(Ipc1)):
+        if Ipc1[i] > Ipc2[i]:
+            result = -1
+            break
+        elif Ipc1[i] < Ipc2[i]:
+            result = 1
+            break
+    return result
+
+# Severity compare
+
+# This function(s_to_impl) calculate a result's Importance level by a given pattern
+# input parameters: result(Severity) and given pattern(Pattern)
+# output: the result's importance level
+# Note that the given pattern should be a dict, in our case it is {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
+# N_req: the number of requirements in results, in out case it is 10
+def s_to_impl(Severity, Pattern, N_req): # severity to ImpL                                                                                    
+    p = [0 for x in range(0,N_req)]
+    for i in range(N_req):
+        if Severity[i] == 0:
+            p[i] = 0
+        else:
+            p[i] = 1
+    N_imp = len(Pattern)
+    l = [0 for x in range(0,N_imp)]
+    for i in range(N_imp):
+        for j in range(len(Pattern[i])):
+            l[i] += p[Pattern[i][j]-1]
+    return tuple(l)
+
+# This function(calc_sv) calculate a result's Severity by a given pattern(Pattern)
+# input parameters: result(s) and given pattern(Pattern)
+# output: the result's Severity(sv)
+# Note that the given pattern should be a dict, in our case it is {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
+# N_req: the number of requirements in results, in out case it is 10
+def calc_sv(s,Pattern):
+    N_imp = len(Pattern)
+    sv = [0 for x in range(0,N_imp)]
+    for i in range(N_imp):
+        for j in range(len(Pattern[i])):
+            sv[i] += s[Pattern[i][j]-1]
+    return sv
+
+# This function(sv_add_s) add a result(s2) to a severity(s1), and return the overall severity
+def sv_add_s(s1,s2, Pattern):
+    N_imp = len(Pattern)
+    for i in range(N_imp):
+        for j in range(len(Pattern[i])):
+            s1[i] += s2[Pattern[i][j]-1]
+    return s1
+
+# This function(comp_sv) compare 2 severity(sv1 and sv2), the returned result is a list:
+# result[0] is the compare result:-1 means sv1 is severer,1 means the otherwise, 0 means sv1 and sv2 are tied
+# result[1] records in which layer sv1 and sv2 are distinguished
+# result[2] records sv1 and sv2's differneces in the layer in which they are distinguished
+def comp_sv(sv1,sv2):
+    result = [0,len(sv1),0]
+    # print("compare_sv:",sv1,sv2)
+    for i in range(len(sv1)):
+        if sv1[i] > sv2[i]:
+            result[0] = -1
+            result[1] = i
+            result[2] = sv1[i] - sv2[i]
+            break
+        elif sv1[i] < sv2[i]:
+            result[0] = 1
+            result[1] = i
+            result[2] = sv2[i] - sv1[i]
+            break
+    return result
 
 # This function(Comp2ADSbySeverity) compare 2 ADS by their testing results' severity:
 # input: ADS1 and ADS2's severity(sv1 and sv2) and their configuration(string, name1 and name2)
@@ -24,8 +102,14 @@ def Comp2ADSbySeverity(sv1,sv2, name1,name2):
 
     impl_sv1 = {}
     impl_sv2 = {}
-    N_req = 10
-    Pattern = {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
+    # N_req = 10
+    # Pattern = {0:(7,), 1:(2,), 2:(3,8), 3:(4,5,6)}
+    f = open(r"E:\UAV\mazda_PP\Hierarchical-Safety-Assessment\Results_RVA\Requirements_Level_Pattern.txt",encoding = "utf-8")
+    f2 = open(r"E:\UAV\mazda_PP\Hierarchical-Safety-Assessment\Results_RVA\N_req.txt",encoding = "utf-8")
+    
+    N_req = eval(f2.read())
+    Pattern = eval(f.read())
+    # print(name1,name2)
     
     for s in sv1:
         impl = s_to_impl(s,Pattern,N_req)
@@ -39,11 +123,12 @@ def Comp2ADSbySeverity(sv1,sv2, name1,name2):
         if impl_sv2.get(impl) == None:
             impl_sv2[impl] = calc_sv(s,Pattern)
         else:
-            impl_sv2[impl] = sv_add_s(impl_sv1[impl], s, Pattern)
+            impl_sv2[impl] = sv_add_s(impl_sv2[impl], s, Pattern)
             
-    N_class = 4
+    N_class = len(Pattern)
     result = 0
-    LEVEL = 4
+    LEVEL = len(Pattern)
+    
     g1 = list(impl_sv1.keys())
     g2 = list(impl_sv2.keys())
     Ms = []
@@ -56,8 +141,8 @@ def Comp2ADSbySeverity(sv1,sv2, name1,name2):
         g = list(set(g))
         g = sorted(g, key=cmp_to_key(ComparebyImportanceClass))
         for t in g:
-            sum_s1 = [0,0,0,0]
-            sum_s2 = [0,0,0,0]
+            sum_s1 = [0 for x in range(0,N_class)]
+            sum_s2 = [0 for x in range(0,N_class)]
             for j in impl_sv1.keys():
                 if j[0:(i+1)] == t:
                     for jj in range(i+1):
@@ -67,6 +152,7 @@ def Comp2ADSbySeverity(sv1,sv2, name1,name2):
                     for jj in range(i+1):
                         sum_s2[jj] += impl_sv2[j][jj]
             cmp_r = comp_sv(sum_s1,sum_s2)
+            
             if(cmp_r[0] != 0):
                 result = cmp_r[0]
                 LEVEL = i+1
@@ -74,16 +160,26 @@ def Comp2ADSbySeverity(sv1,sv2, name1,name2):
                 Msj = cmp_r[1]
                 Dis = cmp_r[2]
                 break
+        
         if(result != 0):
             break
-    df_compare_record = df_compare_record.append({'A':name1,'B':name2,'Compare_result':result,
+        
+    comparison_result = ''
+    if result == 0:
+        comparison_result = 'A=B'
+    elif result == -1: # A is worse than B
+        comparison_result = 'A<B' # which means B is better
+    elif result == 1: # A is better than B
+        comparison_result = 'A>B' # which means A is better
+    
+    df_compare_record = df_compare_record.append({'A':name1,'B':name2,'Compare_result':comparison_result,
                                                   'K': LEVEL, 'Ms': Ms, 
                                                   'j': Msj, 'Dis': Dis},ignore_index=True)
     return result
 
 def getDF(folder):
     df = pd.DataFrame(columns=['Severity'])
-    file = folder + '/' + 'A_resultMod.txt'
+    file = folder + '/' + 'Requirements_Violation_Severity.txt'
     result = np.loadtxt(file)
     for i in range(10000): # len(fileList); 1000-5000-10000
         df.loc[i,'Severity'] = result[i]
@@ -99,20 +195,28 @@ def recordCompareResult(f, folder):
     for i in  range(len(config_list) - 1):
         for j in range((i+1),len(config_list)):
             Comp2ADSbySeverity(df_list[config_list[i]]['Severity'], df_list[config_list[j]]['Severity'],config_list[i],config_list[j] )
-    writer = pd.ExcelWriter('*' + f + '_ALLcompare_result.xlsx',engine = 'xlsxwriter')
+    writer = pd.ExcelWriter('*/Hierarchical-Safety-Assessment/test_result/all_compare/RequirementsViolationComparison_' + f + '.xlsx',engine = 'xlsxwriter')
     df_compare_record.to_excel(writer,sheet_name = 'Sheet1')
     writer.save()
     
 
 if __name__ == '__main__': 
-    datafolder = '*'
 
-    folder_list = ['***']
+    # datafolder = 'E:/UAV/mazda_PP/Revised'
+    # folder_list = ['BlindIntersection_Datalog_2022_01_13_17_33', 'CarOppositeLane_Datalog_2021_12_26_21_25', 'CloseToCrash_Datalog_2021_12_26_21_26', \
+    #               'LeftAndRight_Datalog_2021_12_26_21_26', 'ParkedCarInFrontCarOppositeLane_Datalog_2021_12_26_21_27', 'RightTurn_Datalog_2021_12_26_21_27', \
+    #                 'CarBehindAndInFront_Datalog_2021_12_26_21_25']
     # folder_list.remove('LeftAndRight_Datalog_2021_12_26_21_26')
-    Scenario = ['BlindIntersection','CarOppositeLane', 'CloseToCrash', 'LeftAndRight', 'ParkedCarInFrontCarOppositeLane', 'RightTurn']
+    
+    datafolder = '*/Hierarchical-Safety-Assessment/Results_RVA'
+    folder_list = os.listdir(datafolder)
+    folder_list.remove('README.md')
+    folder_list.remove('Requirements_Level_Pattern.txt')
+    folder_list.remove('N_req.txt')
+
     for f in folder_list:
         print(datafolder + '/' + f)
-        recordCompareResult(f, datafolder + '/' + f + '/New_Revised')
+        recordCompareResult(f, datafolder + '/' + f)
         df_compare_record = df_compare_record.drop(index = df_compare_record.index)
     
 
